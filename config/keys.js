4239e595ccbcefa8ee882ac7d64e58bf41aaa39c
Sanchez <hubabuba627@gmail.com>
2019-05-06 17:42:13 +0300

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://storybooks:ciYp9sC31UQEJaog@sancluster-szqmr.azure.mongodb.net/storybooks?retryWrites=true',
+  googleClientID: '128363131229-1ms0t89r7c2rlj9qutc4evbk2pu04s28.apps.googleusercontent.com',
+  googleClientSecret: 'lXjR-VqdceFYsPNRzjamlJ2e'
+}
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Passport Config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => res.send('It works!'));
 
-const port = process.env.port || 8000;
+//Use Routes
+app.use('/auth', auth);
+
+const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Server run... on port ${port}`));
